7332baca1de6342df651b5925cdd5fb2395738c5
Hailey Magoline <magoline.hailey@gmail.com>
2021-11-30 16:50:19 -0600

    added mouseup and mousedown events on buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './less/index.less'
 
 // Your code goes here!
+const buttons = document.querySelectorAll('.btn');
+
+
+buttons.forEach(button => {
+    button.addEventListener('mousedown', function mouseDown(){
+        button.style.color = 'orange';
+        border.style = '1px solid orange';
+
+    });
+    button.addEventListener('mouseup', function mouseUp(){
+        button.style.color = '#17A2B8';
+    });
+ });
+
+
+
+
