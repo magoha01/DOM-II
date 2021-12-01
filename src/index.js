9faf34dd626e7acfd59846d30b5aba161235ea00
Hailey Magoline <magoline.hailey@gmail.com>
2021-11-30 18:11:57 -0600

    added mouseover, mouseout, and click event

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import './less/index.less'
 
 // Your code goes here!
-const buttons = document.querySelectorAll('.btn');
 
+
+//mousedown and mouseup on bottom buttons
+
+const buttons = document.querySelectorAll('.btn');
 
 buttons.forEach(button => {
     button.addEventListener('mousedown', function mouseDown(){
         button.style.color = 'orange';
-        border.style = '1px solid orange';
-
     });
     button.addEventListener('mouseup', function mouseUp(){
         button.style.color = '#17A2B8';
@@ -16,5 +17,27 @@ buttons.forEach(button => {
  });
 
 
+ //mouseover and mouseout on nav links
+
+ const navLinks = document.querySelectorAll('nav a');
+
+ navLinks.forEach(link => {
+     link.addEventListener('mouseover', function mouseOver(){
+            link.style.fontWeight = 'bold';
+     });
+     link.addEventListener('mouseout', function mouseOut(){
+        link.style.fontWeight = 'normal';
+    });
+ });
+
+//change background color with click event
+
+window.addEventListener('click', () => document.body.style.backgroundColor = '#b4ede0');
 
 
+
+document.addEventListener('keydown', event => {
+    if (32 === event.key) {
+      document.body.style.backgroundColor = 'white';
+    }
+  })
