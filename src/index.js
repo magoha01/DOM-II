eb37bbb4a554bd1051b9cdf637f5ced457d18bac
Hailey Magoline <magoline.hailey@gmail.com>
2021-11-30 20:22:59 -0600

    added wheel event

--- src/index.js
+++ src/index.js
@@ -51,7 +51,26 @@ document.addEventListener('keydown', event => {
     }
   });
 
+//load event
 
-  window.onload = () => {
-    alert('page is fully loaded');
-  };
+  window.addEventListener('onload', () =>  alert('page is fully loaded'));
+
+
+  function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    el.style.transform = `scale(${scale})`;
+  }
+    let scale = 1;
+    const el = document.querySelector('h1');
+
+    el.addEventListener('wheel', zoom);
+
+
+
